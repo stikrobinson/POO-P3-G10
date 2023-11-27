eb75da012755834e2ea5f6956cda2f9eef166f35
Carlos Ronquillo <carrbrus@espol.edu.ec>
2023-11-27 18:18:05 -0500

    Creación clase CuentaRedSocial

---
+++ ProyectoPOO1P/src/main/java/ec/edu/espol/proyectopoo1p/CuentaRedSocial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.proyectopoo1p;
+
+/**
+ *
+ * @author GENERATION 10
+ */
+public class CuentaRedSocial {
+    
+    private String cuenta;
+    private RedSocial redSocial;
+    
+}
