106e38cc05cd873abd19936ab3727c5e61cdc685
Carlos Ronquillo <carrbrus@espol.edu.ec>
2023-11-27 18:14:08 -0500

    Creación clase Auspiciante

---
+++ ProyectoPOO1P/src/main/java/ec/edu/espol/proyectopoo1p/Auspiciante.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.proyectopoo1p;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author GENERATION 10
+ */
+public class Auspiciante extends Participante {
+    private SectorCubierto sector;
+    private ArrayList<Auspiciante> auspiciantes;
+    
+    
+}
