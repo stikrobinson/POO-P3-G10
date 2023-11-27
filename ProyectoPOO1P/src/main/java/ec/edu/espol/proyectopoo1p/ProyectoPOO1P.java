e425852a2bdf6359f0fd9f7a08ee513d01bb77fa
carrbrus <139665813+carrbrus@users.noreply.github.com>
2023-11-27 18:00:34 -0500

    Add files via upload

---
+++ ProyectoPOO1P/src/main/java/ec/edu/espol/proyectopoo1p/ProyectoPOO1P.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.proyectopoo1p;
+
+/**
+ *
+ * @author GENERATION 10
+ */
+public class ProyectoPOO1P {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
