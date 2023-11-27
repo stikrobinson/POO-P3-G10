7d78fe88ee3dde6ab656a96d230a2a931c966951
Carlos Ronquillo <carrbrus@espol.edu.ec>
2023-11-27 18:16:15 -0500

    Creación clase AuspicianteEnFeria

---
+++ ProyectoPOO1P/src/main/java/ec/edu/espol/proyectopoo1p/AuspicianteEnFeria.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.proyectopoo1p;
+
+/**
+ *
+ * @author GENERATION 10
+ */
+public class AuspicianteEnFeria {
+    private Auspiciante auspiciante;
+
+    public Auspiciante getAuspiciante() {
+        return auspiciante;
+    }
+
+    public void setAuspiciante(Auspiciante auspiciante) {
+        this.auspiciante = auspiciante;
+    }
+    
+    
+}
