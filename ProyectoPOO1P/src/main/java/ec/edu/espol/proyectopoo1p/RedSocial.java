0a6afc399bd485c04b560d844e4db6da90f15e0b
Carlos Ronquillo <carrbrus@espol.edu.ec>
2023-11-27 18:25:12 -0500

    Creación clase RedSocial

---
+++ ProyectoPOO1P/src/main/java/ec/edu/espol/proyectopoo1p/RedSocial.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package ec.edu.espol.proyectopoo1p;
+
+/**
+ *
+ * @author GENERATION 10
+ */
+public enum RedSocial {
+    TWITTER,FACEBOOK,INSTAGRAM,YOUTUBE,TIKTOK,LINKEDIN,PINTEREST
+}
