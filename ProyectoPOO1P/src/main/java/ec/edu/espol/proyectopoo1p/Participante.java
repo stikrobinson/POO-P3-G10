bcbcb49e72606341997b11783aa57a1d326ad4b7
Carlos Ronquillo <carrbrus@espol.edu.ec>
2023-11-27 18:24:08 -0500

    Creación clase Participante

---
+++ ProyectoPOO1P/src/main/java/ec/edu/espol/proyectopoo1p/Participante.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.proyectopoo1p;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author GENERATION 10
+ */
+public class Participante {
+    
+    private String cedula;
+    private String telefono;
+    private String email;
+    private String direccion;
+    private String sitio_web;
+    private String nombre_responsable;
+    private ArrayList<CuentaRedSocial> listaRedesSociales;
+    
+}
+
